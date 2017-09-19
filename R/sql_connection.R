e51ce29dd83389e820bc2b20aece0550dad6e8c6
trefoil-ml <hicham@trefoil.ml>
2017-09-19 11:25:46 +0200

    # first change

--- R/sql_connection.R
+++ R/sql_connection.R
@@ -4,5 +4,6 @@
 
 #connection_string <- "Driver=SQL Server;Server=localhost; Database=Hospital;UID=hzmarrou;PWD=trefoil1234"
 connection_string <- "Driver=SQL Server;Server=TREFOILML;Database=Hospital;UID=hzmarrou;PWD=trefoil1234"
+
 sql   <-   RxInSqlServer(connectionString = connection_string)
 local <- RxLocalSeq()
